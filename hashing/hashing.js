a76cc1b6662d589614ff36c54842e402481eba0f
Frieda Abdo <friedabdo@gmail.com>
2021-11-03 14:37:17 -0400

    part 1

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -19,7 +19,8 @@ var Blockchain = {
 	blocks: [],
 };
 
-// Genesis block
+// Genesis block is the only block that is hard coded, the rest will be dynamic
+
 Blockchain.blocks.push({
 	index: 0,
 	hash: "000000",
@@ -28,16 +29,48 @@ Blockchain.blocks.push({
 });
 
 // TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+for (let line of poem) {
+	createBlock(line)
+}
+
+// create a function called createBlock()
+// 'index'
+// 'prevHash'
+// 'data'
+// 'timestamp'
+// 'hash'
+
+function createBlock(_data) {
+	let block = {
+		index: Blockchain.blocks.length,
+		prevHash:Blockchain.blocks[Blockchain.blocks.length-1].hash,
+		data: _data,
+		timestamp: Date.now()
+	}
+
+	//hash has to be outside the block obj bc those are the datas that need to be hashed, then we add the hash to obj at the end
+	block.hash = blockHash(block) 
+	Blockchain.blocks.push(block)
+	// console.log(block)
+	return block
+}
 
 // console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
 
 
 // **********************************
 
-function blockHash(bl) {
+function blockHash(bl) {  
+
+
+	// you can stringify the block before putting it into the return statement func, or call them via string interpolation below
+	// let block = JSON.stringify(bl)
+
 	return crypto.createHash("sha256").update(
 		// TODO: use block data to calculate hash
+		// block // --> can place the stringified variable block (line 63) instead of string interpolation
+		`${bl.index};${bl.prevHash};${bl.data};${bl.timestamp}`
 	).digest("hex");
 }
+
+console.log(Blockchain.blocks)
